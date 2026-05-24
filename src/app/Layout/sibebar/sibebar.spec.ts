dc4c1c831e51b1f6fb2779d248737950d478de1e
RafaelDavidEspinosaSegura <rafael.espinosa@opitech.com.co>
2026-05-24 12:00:58 -0500

    Feature(CuerpoDeAngular)

---
+++ src/app/Layout/sibebar/sibebar.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Sibebar } from './sibebar';
+
+describe('Sibebar', () => {
+  let component: Sibebar;
+  let fixture: ComponentFixture<Sibebar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Sibebar],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Sibebar);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Layout/sibebar/sibebar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sibebar',
+  imports: [],
+  templateUrl: './sibebar.html',
+  styleUrl: './sibebar.css',
+})
+export class Sibebar {}
